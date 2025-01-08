052889d56bc6ea5c579a465788b866d790469b46
AndersonCarvalhoo <andersoncarvalhoo2005@gmail.com>
2025-01-08 17:50:35 -0300

    metodo para gerar dados de fictícios para teste

--- revisandoJavaComNelio/projetoLoja/src/entities/Store.java
+++ revisandoJavaComNelio/projetoLoja/src/entities/Store.java
@@ -8,9 +8,20 @@ public class Store {
 
   ArrayList<Product> products = new ArrayList<>();
 
+  public Store() {
+    this.generateTestData();
+  }
+
   public Store(String name, String phone) {
+    this.generateTestData();
     this.name = name;
     this.phone = phone;
+  }
+
+  private void generateTestData() {
+    products.add(new Product("Maçã", 5.0, 25));
+    products.add(new ClothingProduct("Calça Legging", 79.99, 5, "83988325171", "Algodão"));
+    products.add(new EletronicProduct("Samsung S23", 2900.0, 2, 24));
   }
 
   public String getName() {
