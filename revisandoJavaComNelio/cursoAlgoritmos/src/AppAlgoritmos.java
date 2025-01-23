3e0975b2b385d8806ed9ac33075b8dc1791b3bb8
AndersonCarvalhoo <andersoncarvalhoo2005@gmail.com>
2025-01-23 20:39:46 -0300

    leetcode Palindrome Number

--- revisandoJavaComNelio/cursoAlgoritmos/src/AppAlgoritmos.java
+++ revisandoJavaComNelio/cursoAlgoritmos/src/AppAlgoritmos.java
@@ -1,12 +1,17 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 public class AppAlgoritmos {
     public static void main(String[] args) throws Exception {
         char[] s = { 'h', 'e', 'l', 'l', 'o' };
         reverseString(s);
+        isPalindrome(121);
     }
 
+    // 0 4 (o, h)
+    // 1 3 (o, l, e,h)
+    // 22 não muda porque é o meio
+    // Então a logica é: enquanto o finalIndex for maior que o StartIndex a lógica
+    // continua
+    // Porquê só deve parar caso o startIndex e o finalIndex seja igual e o
+    // finalIndex não pode ser menor que o startIndex
     public static void reverseString(char[] s) {
         int startArray = 0;
         int finalArray = s.length - 1;
@@ -23,5 +28,22 @@ public class AppAlgoritmos {
         for (char teste : s) {
             System.out.println("\n" + teste);
         }
+    }
+
+    public static boolean isPalindrome(int x) {
+        String numberToString = String.valueOf(x);
+        String[] numberToStringSplit = numberToString.split("");
+        String numberInverse = "";
+        for (int i = numberToStringSplit.length - 1; i >= 0; i--) {
+            numberInverse += numberToStringSplit[i];
+        }
+        System.out.println(numberInverse instanceof String);
+        System.out.println(numberToString instanceof String);
+        if (numberInverse.equals(numberToString)) {
+            System.out.println("igual");
+            return true;
+        }
+        System.out.println("não igual");
+        return false;
     }
 }
